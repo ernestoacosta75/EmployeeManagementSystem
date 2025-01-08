5f48ec75d858d41f4827384505c46487e6cc3fef
Ernesto Antonio Rodriguez Acosta <ernestoantonio.rodriguezacosta@msc.com>
2025-01-08 16:01:44 +0100

    Updating appsettings for development. Improving the AppDbContext class.

--- EmployeeManagementSystem.Domain.Services/DatabaseContext/AppDbContext.cs
+++ EmployeeManagementSystem.Domain.Services/DatabaseContext/AppDbContext.cs
@@ -1,9 +1,17 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using EmployeeManagementSystem.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace EmployeeManagementSystem.Domain.Services.DatabaseContext;
 
 public class AppDbContext : DbContext
 {
+    public DbSet<Employee> Employees { get; set; }
+    public DbSet<GeneralDepartment> GeneralDepartments { get; set; }
+    public DbSet<Department> Departments { get; set; }
+    public DbSet<Branch> Branches { get; set; }
+    public DbSet<Town> Towns { get; set; }
+    public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+    
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
         ArgumentNullException.ThrowIfNull(options);
